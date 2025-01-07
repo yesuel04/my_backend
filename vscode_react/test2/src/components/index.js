f9c163a77c354ec6a9683f7dfd09e3f29d85ec75
yesuel <mayesuel@gmail.com>
2025-01-07 17:20:38 +0900

    0107

--- vscode_react/test2/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as About} from './About';
-export {default as Info} from './Info';
-export {default as Main} from './Main';
-export {default as Menu} from './Menu';
-export {default as Title} from './Title';
